6014c0e80f48c48bf712ab43325e17e45b7c38d4
izambrano19 <izambrano19@ilg.cat>
2023-05-12 18:39:08 +0200

    avances12052023

--- src/com/pluralcamp/daw/persistence/daos/impl/jdbc/ColorDAOJDBCImpl.java
+++ src/com/pluralcamp/daw/persistence/daos/impl/jdbc/ColorDAOJDBCImpl.java
@@ -4,6 +4,11 @@ import com.pluralcamp.daw.entities.core.Color;
 import com.pluralcamp.daw.persistence.daos.contracts.ColorDAO;
 import com.pluralcamp.daw.persistence.exceptions.DAOException;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ColorDAOJDBCImpl implements ColorDAO {
@@ -12,11 +17,54 @@ public class ColorDAOJDBCImpl implements ColorDAO {
 
         //Objectes que calen:
         //1er objecte - Connexio, via DriverManager de JDBC
+    	
+    	Connection connection = null;
+    	PreparedStatement sentSQL = null;
+    	ResultSet reader = null;
+    	Color color = null;
+    	
+    	try {
+    		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/calendar?serverTimezone=Europe/Paris", "ismael", "12345678");
+    		sentSQL = connection.prepareStatement("SELECT id, name, red, green, blue FROM colors WHERE id = ?");
+    		sentSQL.setLong(1, id);
+    		reader = sentSQL.executeQuery();
+    		
+    		if(reader.next()) {
+    			color = new Color(reader.getString("name"),reader.getInt("red"), reader.getInt("green"), reader.getInt("blue"));
+    			color.setId(reader.getLong("id"));
+    			
+    		}
+    	}catch(SQLException ex){
+    		
+    	}
+    	finally {
+    		try {
+        		if(reader != null) {
+        			reader.close();
+        		}
+        		
+        		if(sentSQL != null) {
+        			sentSQL.close();
+        		}
+        		
+        		if(connection != null) {
+        			connection.close();
+        		}
+    		}
+    		catch(SQLException ex){
+        		//Logger 
+    			throw new DAOException(ex);
+        	}
+    		
+
+    		
+    	}
+    	
         //2n objecte - Obrir un canal de Consulta - PraparedStatement
         //2.1 - Enviar la consulta SQL
         //3er objecte - Obrir un canal de Lectura, un cursor - ResultSet
 
-        return null;
+        return color;
     }
 
     @Override
--- src/com/pluralcamp/daw/ui/console/App.java
+++ src/com/pluralcamp/daw/ui/console/App.java
@@ -1,7 +1,33 @@
 package com.pluralcamp.daw.ui.console;
+import com.pluralcamp.daw.persistence.daos.impl.jdbc.ColorDAOJDBCImpl;
+import com.pluralcamp.daw.persistence.daos.impl.jdbc.EmployeeDAOJDBCImpl;
+import com.pluralcamp.daw.persistence.daos.impl.jdbc.EventDAOJDBCImpl;
+import com.pluralcamp.daw.persistence.exceptions.DAOException;
+import com.pluralcamp.daw.entities.core.Color;
+
+
+//import java.awt.Color;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello JDBC");
+    	
+    	Scanner stdin = new Scanner(System.in);
+    	
+    	ColorDAOJDBCImpl colorDAOJDBC = new ColorDAOJDBCImpl();
+    	EventDAOJDBCImpl eventDAOJDBC = new EventDAOJDBCImpl();
+
+        try {
+        	Color c = colorDAOJDBC.getColorById(5);
+        	if(c != null) {
+        		System.out.println(c.toString());
+        	}
+        }
+        catch(DAOException ex) {
+        	System.out.printf("Error:: %s %n", ex.getMessage());
+        	
+        }
+        
+        
     }
 }
