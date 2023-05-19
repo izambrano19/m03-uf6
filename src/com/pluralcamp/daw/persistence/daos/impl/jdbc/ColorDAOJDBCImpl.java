3090db974eed106b8eff9ab1f3c594168373f7e8
izambrano19 <izambrano19@ilg.cat>
2023-05-19 16:49:13 +0200

    terminado

--- src/com/pluralcamp/daw/persistence/daos/impl/jdbc/ColorDAOJDBCImpl.java
+++ src/com/pluralcamp/daw/persistence/daos/impl/jdbc/ColorDAOJDBCImpl.java
@@ -6,70 +6,72 @@ import com.pluralcamp.daw.persistence.exceptions.DAOException;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ColorDAOJDBCImpl implements ColorDAO {
     @Override
     public Color getColorById(long id) throws DAOException {
 
-        //Objectes que calen:
+    	 //Objectes que calen:
         //1er objecte - Connexio, via DriverManager de JDBC
-    	
-    	Connection connection = null;
-    	PreparedStatement sentSQL = null;
-    	ResultSet reader = null;
-    	Color color = null;
-    	
-    	try {
-    		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/calendar?serverTimezone=Europe/Paris", "ismael", "12345678");
-    		sentSQL = connection.prepareStatement("SELECT id, name, red, green, blue FROM colors WHERE id = ?");
-    		sentSQL.setLong(1, id);
-    		reader = sentSQL.executeQuery();
-    		
-    		if(reader.next()) {
-    			color = new Color(reader.getString("name"),reader.getInt("red"), reader.getInt("green"), reader.getInt("blue"));
-    			color.setId(reader.getLong("id"));
-    			
-    		}
-    	}catch(SQLException ex){
-    		
-    	}
-    	finally {
-    		try {
-        		if(reader != null) {
-        			reader.close();
-        		}
-        		
-        		if(sentSQL != null) {
-        			sentSQL.close();
-        		}
-        		
-        		if(connection != null) {
-        			connection.close();
-        		}
-    		}
-    		catch(SQLException ex){
-        		//Logger 
-    			throw new DAOException(ex);
-        	}
-    		
-
-    		
-    	}
-    	
         //2n objecte - Obrir un canal de Consulta - PraparedStatement
         //2.1 - Enviar la consulta SQL
         //3er objecte - Obrir un canal de Lectura, un cursor - ResultSet
+    	
+    	Color color = null;
+    	
+    	try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/calendar?serverTimezone=Europe/Paris", "ismael", "12345678");
+    		 CallableStatement sentSQL = connection.prepareCall("CALL getColorById(?)")){
+    		
+    		sentSQL.setLong(1, id);
+    		
+    		try(ResultSet reader = sentSQL.executeQuery()){
+	    		if(reader.next()) {
+	    			color = new Color(reader.getString("name"),reader.getInt("red"), reader.getInt("green"), reader.getInt("blue"));
+	    			color.setId(reader.getLong("id"));
+	    			
+	    		}
+    		}
+    	}catch(SQLException ex){
+    		throw new DAOException(ex);
+    		
+    	}
+
 
         return color;
+    	
     }
 
     @Override
     public List<Color> getColors() throws DAOException {
-        return null;
+        
+    	
+    	List<Color> colors = new ArrayList<>();
+    	
+    	try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/calendar?serverTimezone=Europe/Paris", "ismael", "12345678");
+    		 CallableStatement sentSQL = connection.prepareCall("CALL getColors()");
+    		 ResultSet reader = sentSQL.executeQuery()){
+	    		while(reader.next()) {
+	    			var color = new Color(reader.getString("name"),reader.getInt("red"), reader.getInt("green"), reader.getInt("blue"));
+	    			color.setId(reader.getLong("id"));
+	    			colors.add(color);
+	    			
+	    		}
+    		}
+    	catch(SQLException ex){
+    		throw new DAOException(ex);
+    		
+    	}
+
+
+        return colors;
+        
+   
+        
     }
 
     @Override
--- src/com/pluralcamp/daw/ui/console/App.java
+++ src/com/pluralcamp/daw/ui/console/App.java
@@ -5,7 +5,7 @@ import com.pluralcamp.daw.persistence.daos.impl.jdbc.EventDAOJDBCImpl;
 import com.pluralcamp.daw.persistence.exceptions.DAOException;
 import com.pluralcamp.daw.entities.core.Color;
 
-
+import java.util.List;
 //import java.awt.Color;
 import java.util.Scanner;
 
@@ -27,6 +27,21 @@ public class App {
         	System.out.printf("Error:: %s %n", ex.getMessage());
         	
         }
+        
+        System.out.println("Pulsa una tecla para continuar");
+        stdin.nextLine();
+        
+        try{
+        	List<Color> colors = colorDAOJDBC.getColors();
+        	for(Color c: colors) {
+        		System.out.println(c.toString());
+        	}
+        }catch(DAOException ex) {
+        	System.out.printf("Error:: %s %n", ex.getMessage());
+        }
+        
+        System.out.println("Pulsa una tecla para continuar");
+        stdin.nextLine();
         
         
     }
